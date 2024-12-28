725736b2cb485067153112c2c2ce8417b86293c2
silverspoon2000 <163788999+silverspoon2000@users.noreply.github.com>
2024-12-27 20:50:16 -0500

    Update SimLab2fixed.c

--- SimLab2fixed.c
+++ SimLab2fixed.c
@@ -1,8 +1,25 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
  
 #define BUFSIZE 256
+
+
+void encodeShellString(const char* input, char* output, size_t output_size) {
+    const char* dangerous_chars = ";&|><$`!{}[]()\"'\\";
+    size_t out_index = 0;
+
+    for (size_t i = 0; input[i] != '\0' && out_index < output_size - 1; ++i) {
+        // If the character is dangerous, prepend it with a backslash
+        if (strchr(dangerous_chars, input[i]) != NULL && out_index < output_size - 2) {
+            output[out_index++] = '\\';
+        }
+        // Copy the character to the output buffer
+        output[out_index++] = input[i];
+    }
+    output[out_index] = '\0';
+}
     
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
@@ -15,7 +32,7 @@ int main(int argc, char** argv) {
     char encoded_arg[BUFSIZE] = "";
 
     // Encode the file address input to mitigate command-line injection
-    encodeShellString(argv[1], encoded_arg);
+    encodeShellString(argv[1], encoded_arg, BUFSIZE);
     // strcat(cmd, argv[1]);
     // concatenate using strncat instead of strcat, and check byte size of what's copied to prevent buffer overflow
     if (strncat(cmd, encoded_fileaddress, BUFSIZE - strlen(cmd) - 1) == NULL) {
